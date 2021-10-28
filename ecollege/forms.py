54ef0d53931a89fb9b99bdcc83ebb5b3f99d7f25
unknown <coglescode@yahoo.com>
2021-10-28 16:05:54 +0200

    1st commit backend, style ready

---
+++ ecollege/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.contenttypes import fields
+from django.forms.models import inlineformset_factory
+from . models import Course, Module
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User, Group
+
+
+ModuleFormSet = inlineformset_factory(Course, 
+ Module, fields=['title', 'description'], extra=2, can_delete=True)
+
+
+class UserRegisterForm(UserCreationForm):
+    group = forms.ModelChoiceField(queryset=Group.objects.all(), required=True)
+    
+    class Meta:
+      model = User
+      fields = ['username', 'password1', 'password2', 'group']
---
+++ ecollege/urls.py
@@ -0,0 +1,27 @@
+from django.conf.urls import url
+from django.urls import path
+from . import views
+
+#from ecollege.views import CourseListView
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+  #path('', views.index, name="index"),
+  path('', views.IndexView.as_view(), name="index"),
+  path('my/courses/', views.ManageCourseListView.as_view(), name='manage_course_list'),
+  path('create/', views.CourseCreateView.as_view(), name='course_create'),
+  path('<pk>/edit/', views.CourseUpdateView.as_view(), name='course_edit'),
+  path('<pk>/delete/', views.CourseDeleteView.as_view(), name='course_delete'),
+  path('<pk>/module/', views.CourseModuleUpdateView.as_view(), name='course_module_update'),  
+  path('module/<int:module_id>/content/<model_name>/create/', views.ContentCreateUpdateView.as_view(), name='module_content_create'),
+  path('module/<int:module_id>/content/<model_name>/<id>/', views.ContentCreateUpdateView.as_view(), name='module_content_update'),
+  path('content/<int:id>/delete/', views.ContentDeleteView.as_view(), name='module_content_delete'),
+  path('module/<int:module_id>/', views.ModuleContentListView.as_view(), name='module_content_list'),
+  path('subject/<slug:subject>/', views.CourseListView.as_view(), name='course_list_subject'),
+  path('<slug:slug>/', views.CourseDetailView.as_view(), name='course_detail'), 
+  
+  #path('teacher/register/', views.TeacherRegisterView.as_view(), name='teacher_registration'),
+
+
+]
