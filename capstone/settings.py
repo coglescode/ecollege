e1c507dab0958b037fc11768b33feaddb63791d9
coglescode <19336315+coglescode@users.noreply.github.com>
2021-12-03 00:02:06 +0100

    Update settings.py

--- capstone/settings.py
+++ capstone/settings.py
@@ -33,9 +33,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(e4$bf_d0g(awgsqcp^1mxhheq5mx06_!c*hqo$3u(bjh^ng*s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['coglescode.herokuapp.com']
 
 
 # Application definition
