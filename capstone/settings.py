42847b00adcbe5a512e60548f3160b69b209c0b0
Leonard Cogles Bonne <cogles_bonne@yahoo.com>
2022-10-07 21:34:34 +0200

    Using Postgres instead

--- capstone/settings.py
+++ capstone/settings.py
@@ -88,14 +88,6 @@ WSGI_APPLICATION = 'capstone.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    """
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-    """
-    
     'default': {
         'DATABASE_URL': config('DATABASE_URL'), 
         'PGDATABASE': config('PGDATABASE'),
@@ -103,9 +95,16 @@ DATABASES = {
         'PGPASSWORD': config('PGPASSWORD'),
         'PGPORT': config('PGPORT'),
         'PGUSER': config('PGUSER'),
-    }
+    } 
 }
 
+"""
+'default': {
+'ENGINE': 'django.db.backends.sqlite3',
+'NAME': BASE_DIR / 'db.sqlite3',
+#'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+}
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
