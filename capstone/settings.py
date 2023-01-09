e397d18bb97eeec797b90d3343161ce90baa7ce9
Leonard Cogles Bonne <cogles_bonne@yahoo.com>
2023-01-09 21:34:39 +0100

    Updates

--- capstone/settings.py
+++ capstone/settings.py
@@ -12,12 +12,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
-#import whitenoise.middleware
+import whitenoise.middleware
 from django.urls import reverse_lazy
 
 import os
 import dj_database_url
-#from decouple import config 
+from decouple import config 
 from dotenv import  load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,8 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
+#SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
