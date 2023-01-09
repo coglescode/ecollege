1b352dc835b59d740d3e0ad0fd6b33f00545273e
Leonard Cogles Bonne <cogles_bonne@yahoo.com>
2023-01-09 17:14:34 +0100

    Settings updated

--- capstone/settings.py
+++ capstone/settings.py
@@ -29,10 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('DEBUG', default=True, cast=bool)
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
@@ -90,13 +90,14 @@ WSGI_APPLICATION = 'capstone.wsgi.application'
 DATABASE_URL = config('DATABASE_URL')
    
 DATABASES = {
-    # 'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800), 
-        
-    'default': {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': BASE_DIR / 'db.sqlite3',
-    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800) 
+
+     
+    # 'default': {
+    # 'ENGINE': 'django.db.backends.sqlite3',
+    # 'NAME': BASE_DIR / 'db.sqlite3',
+    # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # } """
     
 }
 
