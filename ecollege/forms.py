1bf7f2480dfa6af7923f1290718ec8348ed517d5
Leonard Cogles Bonne <coglescodes@gmail.com>
2026-05-10 12:00:19 +0200

    Design updates for login page.

--- ecollege/forms.py
+++ ecollege/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.contenttypes import fields
+from django.forms import EmailField
 from django.forms.models import inlineformset_factory
 from . models import Course, Module
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
@@ -14,7 +15,15 @@ ModuleFormSet = inlineformset_factory(Course,
 
 
 class LoginForm(AuthenticationForm):
+
+    username = forms.CharField(label='Your user name:', widget=forms.TextInput(attrs={'class': 'input-focus w-full bg-zinc-50 border border-zinc-200 rounded-2xl py-4 pl-11 pr-4 text-base focus:outline-none focus:border-indigo-500',
+                                                                                    'id': 'USERNAME',
+                                                                                    'placeholder': 'your username',
+                                                                                    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input-focus w-full bg-zinc-50 border border-zinc-200 rounded-2xl py-4 pl-11 pr-12 text-base focus:outline-none focus:border-indigo-500',
+                                                                 'id': 'PASSWORD',
+                                                                 'type': 'password',
+                                                                 'placeholder': '••••••••'}))
+
     def __init__(self, *args, **kwargs):
         super(LoginForm, self).__init__(*args, **kwargs)
-    username = UsernameField(widget=forms.TextInput(attrs={'class': 'form-control shadow-sm '}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control shadow-sm'}))
