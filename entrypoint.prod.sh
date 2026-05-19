de5359107865b3733d76ae3a2afa406fdd78bbe0
Leonard Cogles Bonne <coglescodes@gmail.com>
2026-05-19 11:29:18 +0200

    Updated settings for production.

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -2,13 +2,16 @@
 
 # Make sure the environment settings value is set to production
 # DJANGO_SETTINGS_MODULE=capstone.settings.production
-source /app/venv/bin/activate
+#source /app/venv/bin/activate
 
 #python manage.py collectstatic --noinput
-python manage.py makemigrations --settings=capstone.settings.local --noinput
-python manage.py migrate --settings=capstone.settings.local --noinput
+#python manage.py makemigrations --settings=capstone.settings.local --noinput
+#python manage.py migrate --settings=capstone.settings.local --noinput
 
-python manage.py runserver --settings=capstone.settings.local 0.0.0.0:8000
+python manage.py makemigrations --settings=capstone.settings.prod --noinput
+python manage.py migrate --settings=capstone.settings.prod --noinput
+
+#python manage.py runserver --settings=capstone.settings.production 0.0.0.0:8000
 # if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
 #     python manage.py createsuperuser --noinput || true
 # fi
@@ -22,5 +25,4 @@ python manage.py runserver --settings=capstone.settings.local 0.0.0.0:8000
 #      --protocol=http \
 #      --vacuum \
 
-
-#uwsgi --ini /app/uwsgi.ini
+uwsgi --http :8001 --module capstone.wsgi:application --env DJANGO_SETTINGS_MODULE=capstone.settings.prod
