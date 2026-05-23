12de32c21147368bfe9048b765e75d60d6e64c7a
Leonard Cogles Bonne <coglescodes@gmail.com>
2026-05-23 13:40:12 +0200

    Updates to TeachersRegistration and StudentsRegistration class.

--- usertype/views.py
+++ usertype/views.py
@@ -37,16 +37,18 @@ class StudentRegistrationView(CreateView):
 class TeacherRegisterView(CreateView):
   template_name = 'users/teacher/registration.html'
   form_class = UserRegisterForm
-  success_url = reverse_lazy('manage_course_list') 
+  success_url = reverse_lazy('manage_course_list')
 
   def form_valid(self, form):
     result = super().form_valid(form)
     cd = form.cleaned_data
-    user = authenticate(username=cd['username'], password=cd['password1'])
+    #user = authenticate(username=cd['username'], password=cd['password1'])
+    user= self.object
+
     # Assing intructors to Instructors group for proper persmissions
-    user.is_students = True
+    user.is_teachers = True
     user.save()
-    group = Group.objects.get(name="teachers")
+    group, created = Group.objects.get_or_create(name="teachers")
     user.groups.add(group)
     login(self.request, user)
     return result
