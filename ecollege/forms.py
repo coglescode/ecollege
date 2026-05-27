1ec3b70d7621daa325af30993f7e6d5385f7e8f6
Leonard Cogles Bonne <coglescodes@gmail.com>
2026-05-27 10:50:43 +0200

    Teacher CMS redesigned.

--- ecollege/forms.py
+++ ecollege/forms.py
@@ -2,15 +2,30 @@ from django import forms
 from django.contrib.contenttypes import fields
 from django.forms import EmailField
 from django.forms.models import inlineformset_factory
-from . models import Course, Module
+from . models import Course, Module, Subject
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
 from django.contrib.auth.models import User, Group
 
 
+class SubjectForm(forms.ModelForm):
+  # Override title to be a ModelChoiceField populated by the Subject model
+  title = forms.ModelChoiceField(
+    queryset=Subject.objects.all(),
+    empty_label="--- Select a Subject ---",
+    widget=forms.Select(attrs={'class': 'form-control'})  # You can add CSS classes here!
+  )
 
+  class Meta:
+    model = Subject
+    fields = ['title']
 
-ModuleFormSet = inlineformset_factory(Course, 
-  Module, fields=['title', 'description'], extra=2, can_delete=True)
+ModuleFormSet = inlineformset_factory(
+  Course,
+  Module,
+  #fields=['title', 'description'],
+  form=SubjectForm,
+  extra=2,
+  can_delete=True)
 
 
 
